a00c994daa7fb18f08e779a7b53e0c1f514d57c0
Dekel Cohen <dcohen52@gmail.com>
2023-05-02 15:15:16 +0300

    Added CSS-link Example.

---
+++ Examples/CSS link/main.py
@@ -0,0 +1,15 @@
+from MVCactus.MVCactus import MVCactus, MVCactusRun
+from random import randint
+
+
+class MyApp(MVCactus):
+
+    @MVCactus.route('/')
+    def index(self, match):
+        self.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app = MyApp
+    server = MVCactusRun(port=randint(1000, 9999))
+    server.run(app)
